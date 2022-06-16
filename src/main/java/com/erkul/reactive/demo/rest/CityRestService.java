9b053acf9be7570b170339faf8a4965da27b031c
erkulm <erkul@itu.edu.tr>
2022-06-16 18:28:56 +0300

    city rest service now using functional style response entities

--- src/main/java/com/erkul/reactive/demo/rest/CityRestService.java
+++ src/main/java/com/erkul/reactive/demo/rest/CityRestService.java
@@ -4,6 +4,7 @@ import com.erkul.reactive.demo.model.CityDTO;
 import com.erkul.reactive.demo.service.CityService;
 import com.erkul.reactive.demo.service.model.CitySearch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
@@ -14,18 +15,18 @@ public class CityRestService {
     private final CityService cityService;
 
     @GetMapping
-    public Flux<CityDTO> getAllCities() {
-        return cityService.getCities();
+    public ResponseEntity<Flux<CityDTO>> getAllCities() {
+        return ResponseEntity.ok(cityService.getCities());
     }
 
     @GetMapping("/get-by-code")
-    public Flux<CityDTO> getCities(@PathVariable String search) {
-        return cityService.searchCities(search);
+    public ResponseEntity<Flux<CityDTO>> getCities(@RequestParam String code) {
+        return ResponseEntity.ok(cityService.searchCities(code));
     }
 
     @GetMapping("/search")
-    public Flux<CityDTO> getCities(@RequestBody CitySearch search) {
-        return cityService.searchCities(search);
+    public ResponseEntity<Flux<CityDTO>> getCities(@RequestBody CitySearch search) {
+        return ResponseEntity.ok(cityService.searchCities(search));
     }
 
 }
